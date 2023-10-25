5bdc56b764e5d597df01daaa09dae5759a30008e
aaaz425 <aaaz425@naver.com>
2023-10-25 17:01:59 +0900

    refactor: 불필요한 위치 계산 과정 삭제
    - 위치 계산 관련 useState, useEffect, useRef 삭제
    - transform으로 대체

--- fe/src/components/Modal/Modal.tsx
+++ fe/src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { Dim } from '@styles/common';
-import React, { LegacyRef, useEffect, useRef, useState } from 'react';
+import React, { LegacyRef } from 'react';
 import { createPortal } from 'react-dom';
 import { styled } from 'styled-components';
 
@@ -11,26 +11,11 @@ type ModalProps = {
 
 const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
   ({ header, content }, ref) => {
-    const modalRef = useRef<HTMLDivElement | null>(null);
-    const [left, setLeft] = useState(0);
-    const [top, setTop] = useState(0);
-
-    useEffect(() => {
-      const modalElement = modalRef.current;
-      const modalWidth = modalElement!.clientWidth;
-      const modalHeight = modalElement!.clientHeight;
-
-      setLeft(modalWidth / 2);
-      setTop(modalHeight / 2);
-    }, [ref]);
-
     return createPortal(
       <Dim>
-        <StyledModal ref={ref} $left={left} $top={top}>
-          <ModalContainer ref={modalRef}>
-            <ModalHeader>{header}</ModalHeader>
-            {content}
-          </ModalContainer>
+        <StyledModal ref={ref}>
+          <ModalHeader>{header}</ModalHeader>
+          {content}
         </StyledModal>
       </Dim>,
       document.getElementById('modal-root')!
@@ -38,34 +23,25 @@ const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
   }
 );
 
-const StyledModal = styled.div<{ $left: number; $top: number }>`
+const StyledModal = styled.div`
   min-width: 20rem;
   position: fixed;
-  top: ${({ $top }) => `calc(50% - ${$top}px)`};
-  left: ${({ $left }) => `calc(50% - ${$left}px)`};
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 2rem;
+  padding: 2rem;
   border-radius: ${({ theme: { radius } }) => radius.medium};
   background-color: ${({ theme: { color } }) => color.neutralBackground};
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  transition:
-    top 0.3s,
-    left 0.3s;
-`;
-
-const ModalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
-  padding: 2rem;
 `;
 
 const ModalHeader = styled.span`
-  font-size: ${({ theme: { fontSize } }) => fontSize.small};
+  font-size: ${({ theme: { fontSize } }) => fontSize.sMedium};
 `;
 
 export default Modal;

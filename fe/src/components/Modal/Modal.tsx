bed42c817b32397b20b30673c44eca6cb3a3680e
aaaz425 <aaaz425@naver.com>
2023-10-25 12:05:26 +0900

    refactor: 모달 공통 컴포넌트 위치 계산 로직 추가

--- fe/src/components/Modal/Modal.tsx
+++ fe/src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { Dim } from '@styles/common';
-import React, { LegacyRef } from 'react';
+import React, { LegacyRef, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { styled } from 'styled-components';
 
@@ -11,11 +11,26 @@ type ModalProps = {
 
 const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
   ({ header, content }, ref) => {
+    const modalRef = useRef<HTMLDivElement | null>(null);
+    const [left, setLeft] = useState(0);
+    const [top, setTop] = useState(0);
+
+    useEffect(() => {
+      const modalElement = modalRef.current;
+      const modalWidth = modalElement!.clientWidth;
+      const modalHeight = modalElement!.clientHeight;
+
+      setLeft(modalWidth / 2);
+      setTop(modalHeight / 2);
+    }, [ref]);
+
     return createPortal(
       <Dim>
-        <StyledModal ref={ref}>
-          <ModalHeader>{header}</ModalHeader>
-          {content}
+        <StyledModal ref={ref} $left={left} $top={top}>
+          <ModalContainer ref={modalRef}>
+            <ModalHeader>{header}</ModalHeader>
+            {content}
+          </ModalContainer>
         </StyledModal>
       </Dim>,
       document.getElementById('modal-root')!
@@ -23,20 +38,30 @@ const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
   }
 );
 
-const StyledModal = styled.div`
-  width: 20rem;
+const StyledModal = styled.div<{ $left: number; $top: number }>`
+  min-width: 20rem;
   position: fixed;
-  top: calc(50% - 10rem);
-  left: calc(50% - 10rem);
+  top: ${({ $top }) => `calc(50% - ${$top}px)`};
+  left: ${({ $left }) => `calc(50% - ${$left}px)`};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 1rem;
-  padding: 2rem 0;
   border-radius: ${({ theme: { radius } }) => radius.medium};
   background-color: ${({ theme: { color } }) => color.neutralBackground};
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  transition:
+    top 0.3s,
+    left 0.3s;
+`;
+
+const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  padding: 2rem;
 `;
 
 const ModalHeader = styled.span`

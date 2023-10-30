3ec42193854958620fde66438e5fe71fd98e797e
aaaz425 <aaaz425@naver.com>
2023-10-30 10:29:28 +0900

    fix: 홈 페이지 음악 파일 상대 경로 수정

--- fe/src/components/Header/HomeHeader.tsx
+++ fe/src/components/Header/HomeHeader.tsx
@@ -23,7 +23,7 @@ export default function HomeHeader() {
     togglePlayingSound,
     sound: HomeBgm,
   } = useSound({
-    src: '../src/assets/bgm/home.mp3',
+    src: '/home.mp3',
   });
 
   const handleLogout = async () => {

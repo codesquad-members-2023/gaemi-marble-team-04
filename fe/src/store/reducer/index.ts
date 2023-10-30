1690f6a0b5433a8e3ad73f3d6372eed666ab7159
aaaz425 <aaaz425@naver.com>
2023-10-30 16:21:23 +0900

    feat: 게임 상태 setter 함수 추가

--- fe/src/store/reducer/index.ts
+++ fe/src/store/reducer/index.ts
@@ -20,4 +20,5 @@ export const useGameInfoValue = () => useAtomValue(gameInfoAtom);
 export const usePlayersValue = () => useAtomValue(playersAtom);
 export const useStocksValue = () => useAtomValue(stocksAtom);
 
+export const useSetGame = () => useSetAtom(gameAtom);
 export const useSetPlayers = () => useSetAtom(playersAtom);
